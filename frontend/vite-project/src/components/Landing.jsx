7ab273c55a40147f447fd7aa7c314522e4d98407
Shivansh pratap singh chauhan <96882189+shivansh-sh@users.noreply.github.com>
2023-08-20 10:32:40 +0530

    some css for admin landing page

--- frontend/vite-project/src/components/Landing.jsx
+++ frontend/vite-project/src/components/Landing.jsx
@@ -9,15 +9,15 @@ function Landing() {
       
     <div style={{display: "flex"}}>
         <div style={{
-            fontFamily: 'monospace', fontSize: "28px", fontWeight: 700, letterSpacing: '.3rem', marginLeft: "120px", marginTop: "120px"
+            fontFamily: 'monospace', fontSize: "28px", fontWeight: 700, letterSpacing: '.3rem', marginLeft: "100px", marginTop: "100px"
         }}>
         <h2 >Welcome to Coursify</h2> 
         <p style={{fontSize:24, marginLeft: 35}}> Be a 100x Developer🚀</p>
         
         </div>
-        <img style={{height: 300, width: 350, marginTop: 100, marginLeft: 170 , borderRadius: 10}} src={pic1} alt="" />
+        <img style={{height: 300, width: 350, marginTop: 120, marginLeft: 180 , borderRadius: 10}} src={pic1} alt="" />
     </div>
-        <div style={{marginTop: -70, marginLeft: 210}}> <p style={{fontSize:22 ,fontFamily: 'monospace', fontWeight: 600, letterSpacing: '.1rem'}}>Are you a new admin?</p>
+        <div style={{marginTop: -110, marginLeft: 210}}> <p style={{fontSize:22 ,fontFamily: 'monospace', fontWeight: 600, letterSpacing: '.1rem'}}>Are you a new admin?</p>
         <Button style={{height: 35,marginTop:18, marginLeft:50,borderRadius: 20}} onClick={()=> {navigate("/register")}} variant="contained" >Click here</Button> 
         </div>
       
